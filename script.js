e747836056de5dc3e93f48dde919fd0334837a6f
Naikii24 <905266@stud.unive.it>
2025-04-05 16:03:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Esempio di evento
-var marker = L.marker([45.4642, 9.1900]).addTo(map) // Milano
-    .bindPopup("<b>Concerto Rock</b><br>Stasera alle 21:00<br><a href='#'>Acquista biglietti</a>")
-    .openPopup();
+fetch("https://docs.google.com/spreadsheets/d/https://docs.google.com/spreadsheets/d/1sz5yRujumvBDYH6qGjKUhJ0Q2vBbQe1aZB4pUZg-oSE/edit?usp=sharing/gviz/tq?tqx=out:json")
+    .then(res => res.text())
+    .then(data => {
+        let json = JSON.parse(data.substr(47).slice(0, -2));
+        json.table.rows.forEach(row => {
+            let nome = row.c[0].v;
+            let lat = parseFloat(row.c[1].v);
+            let lng = parseFloat(row.c[2].v);
+            let desc = row.c[3].v;
+            let link = row.c[4].v;
+
+            L.marker([lat, lng]).addTo(map)
+                .bindPopup(`<b>${nome}</b><br>${desc}<br><a href='${link}' target='_blank'>Biglietti</a>`);
+        });
+    });
