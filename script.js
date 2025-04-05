3fcae1ecf10f75f1f4471ebe0af2fda5af62419a
Naikii24 <905266@stud.unive.it>
2025-04-05 16:28:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Inizializza la mappa
 var map = L.map('map').setView([41.9028, 12.4964], 6); // Italia (Latitudine e Longitudine)
 
+// Aggiungi la mappa di base
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
@@ -17,8 +18,9 @@ fetch("https://docs.google.com/spreadsheets/d/1sz5yRujumvBDYH6qGjKUhJ0Q2vBbQe1aZ
             let desc = row.c[3].v;
             let link = row.c[4].v;
 
+            // Aggiungi un marker per ogni evento
             L.marker([lat, lng]).addTo(map)
                 .bindPopup(`<b>${nome}</b><br>${desc}<br><a href='${link}' target='_blank'>Biglietti</a>`);
         });
-    });
+    })
     .catch(err => console.error('Errore nel caricamento dei dati:', err));
